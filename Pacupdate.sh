4b90ff9fde4b7bf691f72dc26ab1edd062a181f7
SHIVAM MEHROTRA <31403228+mehrotsh@users.noreply.github.com>
2024-08-01 13:07:52 +0530

    Update Pacupdate.sh

--- Pacupdate.sh
+++ Pacupdate.sh
@@ -4,13 +4,17 @@
 kubectl get prometheusrules --all-namespaces -o json | jq -c '.items[]' | while read -r pac; do
     namespace=$(echo "$pac" | jq -r '.metadata.namespace')
     name=$(echo "$pac" | jq -r '.metadata.name')
-    
-    rules=$(echo "$pac" | jq -r '.spec.rules')
-    updated_rules=$(echo "$rules" | jq 'map(if .description then .description |= sub("[.]?$"; ".") else . end)')
+
+    groups=$(echo "$pac" | jq -r '.spec.groups')
+    updated_groups=$(echo "$groups" | jq 'map(
+        .rules |= map(
+            if .description then .description |= sub("[.]?$"; ".") else . end
+        )
+    )')
 
     # Create a temporary JSON file with the updated PAC
     tmpfile=$(mktemp)
-    echo "$pac" | jq --argjson rules "$updated_rules" '.spec.rules = $rules' > "$tmpfile"
+    echo "$pac" | jq --argjson groups "$updated_groups" '.spec.groups = $groups' > "$tmpfile"
 
     # Apply the updated PAC
     kubectl apply -f "$tmpfile" -n "$namespace"
